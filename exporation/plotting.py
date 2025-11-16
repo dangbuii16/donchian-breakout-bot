ccda2e2fc1d8e1026d1beccb0b82c6ea9ad0fd5e
dangbuii16 <dangbui.167@gmail.com>
2025-11-16 18:11:05 +0700

    Add files via upload

--- exporation/plotting.py
+++ exporation/plotting.py
@@ -1,5 +1,6 @@
 import datetime as dt
 import plotly.graph_objects as go
+import pandas as pd
 
 class CandlePlot:
 
@@ -9,6 +10,9 @@ class CandlePlot:
         self.create_candle_fig()
 
     def create_candle_fig(self):
+        """ 
+        Creat candle chart from price data
+        """
         self.fig = go.Figure()
         if self.candles == True:
             self.fig.add_trace(go.Candlestick(
@@ -32,7 +36,7 @@ class CandlePlot:
             gridcolor='#f0f0f0',
             rangeslider_visible=True
         )
-        
+    
     def update_layout(self, width=1480, height=650):
         
         self.fig.update_yaxes(
@@ -54,6 +58,9 @@ class CandlePlot:
         )
 
     def add_traces(self, line_traces):
+        """ 
+        Add line traces to chart (EMA, SMA, Donchian high/low, .etc)
+        """
         for line in line_traces:
             self.fig.add_trace(go.Scatter(
                 x=self.df_plot['time'],
@@ -62,6 +69,113 @@ class CandlePlot:
                 line_shape='spline',
                 name=line
             ))
+
+    def add_trade_markers(self, trades_df, show_entry=True, show_exit=True):
+        """
+        Add trade entry/exit markers on top of the candlestick chart.
+        """
+        if trades_df is None or len(trades_df) == 0:
+            return
+
+        
+        trades = trades_df.copy()
+        trades['entry_time'] = pd.to_datetime(trades['entry_time'])
+        trades['exit_time']  = pd.to_datetime(trades['exit_time'])
+
+        buy_trades  = trades[trades['side'] == "BUY"]
+        sell_trades = trades[trades['side'] == "SELL"]
+
+        # Entry markers 
+        if show_entry:
+            # BUY entry:
+            if not buy_trades.empty:
+                self.fig.add_trace(
+                    go.Scatter(
+                        x=buy_trades['entry_time'],
+                        y=buy_trades['entry_price'],
+                        mode='markers',
+                        name='Buy Entry',
+                        marker=dict(
+                            symbol='triangle-up',
+                            size=10,
+                            color='#24A06B',
+                            line=dict(width=1, color='#1b5136')
+                        ),
+                        hovertemplate=(
+                            "Buy Entry<br>"
+                            "Time: %{x}<br>"
+                            "Price: %{y:.5f}<extra></extra>"
+                        )
+                    )
+                )
+            # SELL entry:
+            if not sell_trades.empty:
+                self.fig.add_trace(
+                    go.Scatter(
+                        x=sell_trades['entry_time'],
+                        y=sell_trades['entry_price'],
+                        mode='markers',
+                        name='Sell Entry',
+                        marker=dict(
+                            symbol='triangle-down',
+                            size=10,
+                            color='#CC2E3C',
+                            line=dict(width=1, color='#6d1b23')
+                        ),
+                        hovertemplate=(
+                            "Sell Entry<br>"
+                            "Time: %{x}<br>"
+                            "Price: %{y:.5f}<extra></extra>"
+                        )
+                    )
+                )
+
+        # Exit markers
+        if show_exit:
+            if not buy_trades.empty:
+                self.fig.add_trace(
+                    go.Scatter(
+                        x=buy_trades['exit_time'],
+                        y=buy_trades['exit_price'],
+                        mode='markers',
+                        name='Buy Exit',
+                        marker=dict(
+                            symbol='circle',
+                            size=8,
+                            color='#7fd8a9',
+                            line=dict(width=1, color='#1b5136')
+                        ),
+                        hovertemplate=(
+                            "Buy Exit<br>"
+                            "Time: %{x}<br>"
+                            "Price: %{y:.5f}<br>"
+                            "PnL: %{customdata:.2f}$<extra></extra>"
+                        ),
+                        customdata=buy_trades['profit_ac']
+                    )
+                )
+            if not sell_trades.empty:
+                self.fig.add_trace(
+                    go.Scatter(
+                        x=sell_trades['exit_time'],
+                        y=sell_trades['exit_price'],
+                        mode='markers',
+                        name='Sell Exit',
+                        marker=dict(
+                            symbol='circle',
+                            size=8,
+                            color='#f29ca6',
+                            line=dict(width=1, color='#6d1b23')
+                        ),
+                        hovertemplate=(
+                            "Sell Exit<br>"
+                            "Time: %{x}<br>"
+                            "Price: %{y:.5f}<br>"
+                            "PnL: %{customdata:.2f}$<extra></extra>"
+                        ),
+                        customdata=sell_trades['profit_ac']
+                    )
+                )
 
     def show_plot(self, width=1480, height=650, line_traces=[]):
         self.add_traces(line_traces)
